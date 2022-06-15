00b2816a51b347eca5f33bda7597f93a98d16f18
kirti shukla <104082719+shukla2004@users.noreply.github.com>
2022-06-15 16:22:31 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int i, s, n;
+	printf("enter the number: ");
+	scanf("%d", &n);
+	for (i = 0; i <= n; i++)
+	{
+
+		s = i * i;
+	}
+	printf(" square of the number:%d", s);
+
+	getch();
+}
